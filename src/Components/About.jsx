3932549e3ece094273e6ee98de28de8fa289aada
Ayushi Awasthi <116361036+Ayushi3023@users.noreply.github.com>
2023-12-08 20:35:48 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,22 +21,20 @@ import image from "../images/motion-background.jpg";
 const imageAltText = "purple and blue abstract background";
 
 /**
- * Sort description that expands on your title on the Home component.
+ * Sort a description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a pre-final student studying at Amity University. I enjoy creating websites and analyzing data.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Data Analysis",
+  "Generative AI",
+  "Machine Learning",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+   "I am passionate about solving problems in new creative ways to drive innovation. I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
