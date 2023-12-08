33a77d7d159847cd9da8d813977e37b02afe6040
Ayushi Awasthi <116361036+Ayushi3023@users.noreply.github.com>
2023-12-08 20:36:53 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Resume builder",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed a resume builder with HTML, CSS, Javascript, and Bootstrap framework to give users a user-friendly way to build effective resumes using preloaded templates.",
+    url: "https://github.com/Ayushi3023/resume-builder",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Portfolio",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Built a portfolio website using HTML5, CSS3, JavaScript, and Bootstrap to showcase projects and skills.",
+    url: "https://ayushi3023.github.io/Portfolio/",
   },
   {
-    title: "My Resume Site",
+    title: "Face mask detector",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Implemented a computer vision model to identify faces in a given video frame using OpenCV, and then provide recognition of the face using the pre-trained model in TensorFlow to analyze the facial features.",
+    url: "https://github.com/Ayushi3023/Face-mask-detection",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Content writing at GeeksforGeeks",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Have written many insightful articles on GeeksforGeeks on topics like Data Structures, Algorithms, and Machine Learning.",
+    url: "https://auth.geeksforgeeks.org/user/ayushi_awasthi_/articles",
   },
 ];
 
